2c04725da0e75eea6e8327aa69d40e9b266678a2
Walter Alcantara <waltermalcantara@gmail.com>
2021-02-14 15:19:46 -0300

    TabNav turned into a class

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
+import TabNav from './modules/tabnav.js';
 
 import initAnimacaoScroll from './modules/scroll-animacao.js'
-import initTabNav from './modules/tabnav.js'
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
@@ -18,8 +18,12 @@ scrollSuave.init();
 const accordionList = new Accordion('[data-anime="accordion"] dt');
 accordionList.init();
 
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+tabNav.init();
+
+
 initAnimacaoScroll();
-initTabNav();
+
 initModal();
 initTooltip();
 initDropdownMenu();
