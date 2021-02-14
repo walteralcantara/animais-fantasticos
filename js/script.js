dfc5ad98e9de7f451d7ab14aa75ccb11bc80c92f
Walter Alcantara <waltermalcantara@gmail.com>
2021-02-14 20:52:34 -0300

    animaisApi.json repaired

--- js/script.js
+++ js/script.js
@@ -40,7 +40,7 @@ menuMobile.init();
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
 
-fetchAnimais('./animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisApi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
 
 
